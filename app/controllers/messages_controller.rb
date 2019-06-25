8dd27e9698489e1efac56b4c42bf74bd21886c41
shohei yamada <saka1212vn@gmail.com>
2019-06-25 14:56:11 +0900

    test

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,7 @@ class MessagesController < ApplicationController
       redirect_to do |format|
         format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
         format.json
+        img.resize "800*800"
       end
     else
       @messages = @group.messages.includes(:user)
@@ -30,3 +31,33 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 end
+
+# class MessagesController < ApplicationController
+#   before_action :set_group
+
+#   def index
+#     @message = Message.new
+#     @messages = @group.messages.includes(:user)
+#   end
+
+#   def create
+#     @message = @group.messages.new(message_params)
+#     if @message.save
+#       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+#     else
+#       @messages = @group.messages.includes(:user)
+#       flash.now[:alert] = 'メッセージを入力してください。'
+#       render :index
+#     end
+#   end
+
+#   private
+
+#   def message_params
+#     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+#   end
+
+#   def set_group
+#     @group = Group.find(params[:group_id])
+#   end
+# end

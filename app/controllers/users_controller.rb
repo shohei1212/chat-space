7de65465559566126c70a76cc4990c146510ce39
shohei yamada <saka1212vn@gmail.com>
2019-06-30 13:10:30 +0900

    incri

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?) and id != ?', "%#{params[:keyword]}%" ,current_user)
     respond_to do |format|
       format.html
       format.json
-    end
+   end
   end
 
   def edit
-    
   end
 
   def update
@@ -26,5 +25,3 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email)
   end
 end
-
-
